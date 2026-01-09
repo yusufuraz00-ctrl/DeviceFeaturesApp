3193276b570533cb87fbd7c122bf07e4804306f3
yusufuraz00-ctrl <yusufuraz00@gmail.com>
2026-01-09 23:49:16 +0300

    Initial commit
    Generated by create-expo-app 3.5.3.

---
+++ App.js
@@ -0,0 +1,19 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import CameraScreen from './screens/CameraScreen';
+import LocationScreen from './screens/LocationScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Camera" component={CameraScreen} />
+        <Stack.Screen name="Location" component={LocationScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
